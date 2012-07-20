82028870455f211ed074c7962426eeb036a1c07c
Bryan Berry <bryan.berry@gmail.com>
2012-07-20 13:28:59 +0200

    basic recipe works

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,9 @@
 default['jmxtrans']['install_prefix'] = '/opt'
 default['jmxtrans']['home'] = "#{node['jmxtrans']['install_prefix']}/jmxtrans"
+default['jmxtrans']['log_dir'] = '/var/log/jmxtrans'
 default['jmxtrans']['user'] = 'jmxtrans'
-default['jmxtrans']['url'] = ''
-default['jmxtrans']['checksum'] = ''
+default['jmxtrans']['url'] = 'http://jmxtrans.googlecode.com/files/jmxtrans-250.zip'
+default['jmxtrans']['checksum'] = 'd6d58b3710046aefaf039d5f921d73ca5b66de2299a572e054a2d70638837e02'
 default['jmxtrans']['heap_size'] = '512'
 default['jmxtrans']['run_interval'] = '60'
 default['jmxtrans']['graphite']['host'] = 'graphite'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,70 @@
 # Apache 2.0 license
 #
 
+include_recipe "logrotate"
+
+if platform_family? "debian"
+  init_script = "jmxtrans.init.deb.erb"
+elsif "rhel"
+  init_script_file = "jmxtrans.init.el.erb"
+end
+
+user node['jmxtrans']['user']
+
+ark "jmxtrans" do
+  url node[:jmxtrans][:url]
+  checksum node[:jmxtrans][:checksum]
+  version "250"
+  prefix_root '/opt'
+  prefix_home '/opt'
+  owner node['jmxtrans']['user']
+  group node['jmxtrans']['user']
+end
+
+template "/etc/init.d/jmxtrans" do
+  source init_script_file
+  owner "root"
+  group "root"
+  mode  "0755" 
+ notifies :restart, "service[jmxtrans]"
+end
+
+template "/etc/default/jmxtrans" do
+  source "jmxtrans_default.erb"
+  owner "root"
+  group "root"
+  mode  "0644"
+  notifies :restart, "service[jmxtrans]"
+end
+
+directory node['jmxtrans']['log_dir'] do
+  owner node['jmxtrans']['user']
+  group node['jmxtrans']['user']
+  mode  "0755"
+end
+
+directory "#{node['jmxtrans']['home']}/json" do
+  owner node['jmxtrans']['user']
+  group node['jmxtrans']['user']
+  mode  "0755"
+end
+
+
+logrotate_app "jmxtrans" do
+  cookbook "logrotate"
+  path "#{node['jmxtrans']['log_dir']}/*.log"
+  frequency "daily"
+  rotate 7
+  create "644 #{node['jmxtrans']['user']} #{node['jmxtrans']['user']} "
+end
+
+# the init script uses this command
+link "/usr/bin/jps"  do
+  to "#{node['java']['java_home']}/bin/jps"
+end
+
+service "jmxtrans" do
+  supports :restart => true, :status => true, :reload => true
+  action [ :enable, :start]
+end
+
